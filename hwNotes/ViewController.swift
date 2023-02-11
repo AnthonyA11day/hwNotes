6f3fb694bb09ad8545f1fc35bbcba1383702226c
Anthony <AvdeyAV@gmail.com>
2023-02-11 15:31:48 +0600

    add UINavigationController add right barButton

--- hwNotes/ViewController.swift
+++ hwNotes/ViewController.swift
@@ -11,7 +11,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        addSubview()
+    }
+    
+    func addSubview() {
+        title = "Notes"
         view.backgroundColor = .systemGray4
+        
+        addBarButtonsFirstVC()
+    }
+}
+
+extension ViewController {
+    func addBarButtonsFirstVC() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add,
+                                                            target: self,
+                                                            action: #selector(actionRightBarButton))
+    }
+    
+    @objc func actionRightBarButton() {
+        print("action right addBarButtonsFirstVC")
     }
 }
 
