05f27c2c52ca58b0354aa616b5cf039ddb427d41
Anthony <AvdeyAV@gmail.com>
2023-02-11 18:20:44 +0600

    add left bar button add func to mass deleter row(cell)

--- hwNotes/ViewController.swift
+++ hwNotes/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewController: UIViewController {
     
     var tableView = UITableView()
+    let identifire = "cell"
+    var array = ["1-","2-","3-","4-","5-","6-","7-","8-","9-"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,11 +31,21 @@ class ViewController: UIViewController {
 //MARK: - bar buttons
 extension ViewController {
     func addBarButtonsFirstVC() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit,
+                                                            target: self,
+                                                            action: #selector(actionLeftBarButton))
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add,
                                                             target: self,
                                                             action: #selector(actionRightBarButton))
+
     }
     
+    
+    @objc func actionLeftBarButton() {
+        print("action left addBarButtonsFirstVC")
+        tableView.isEditing = !tableView.isEditing
+        
+    }
     @objc func actionRightBarButton() {
         print("action right addBarButtonsFirstVC")
         let secondViewController = SecondViewController()
@@ -42,12 +54,17 @@ extension ViewController {
 }
 
 //MARK: - tableView
-extension ViewController {
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     func setupTableView() {
         tableView = UITableView(frame: view.bounds, style: .plain)
         tableView.backgroundColor = .white
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: identifire)
+        //        tableView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         view.addSubview(tableView)
         
         NSLayoutConstraint.activate([
@@ -56,7 +73,43 @@ extension ViewController {
             tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8),
             tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16)
         ])
+    }
+    
+    //MARK: UITableView DataSource methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifire, for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row) test text "
+        cell.textLabel?.text = array[indexPath.row]
+        return cell
+    }
+    
+    //MARK: UITableView Delegate methods
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50.0
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        let number = indexPath.row
+        array.append(String(number))
+        print(number, array.count)
         
     }
     
+    //cell delete editing
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+    
+    //cell delete
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            array.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .left)
+        }
+    }
 }
+
